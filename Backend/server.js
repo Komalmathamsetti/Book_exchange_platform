8ffd71a044b41280609217608f461c81b498b26d
Komal Mathamsetti <komalmathamsetti@gmail.com>
2026-03-11 22:26:34 +0530

    Database setup and connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Backend server is running");
+});
+app.get("/test-db", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows);
 });
 const PORT = 5000;
 app.listen(PORT,()=>{
