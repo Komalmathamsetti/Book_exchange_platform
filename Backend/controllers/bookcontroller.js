866c6f382b86fcda5998f0a9b21b0708bca64e62
Komal Mathamsetti <komalmathamsetti@gmail.com>
2026-03-14 23:11:36 +0530

    Add and update and get single book API's

--- Backend/controllers/bookcontroller.js
+++ Backend/controllers/bookcontroller.js
@@ -36,3 +36,35 @@ exports.deleteBook = async(req,res)=>{
         res.status(500).send("Server error");
     }
 };
+exports.getBookById = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const result = await pool.query("SELECT * FROM books WHERE id = $1",[id]);
+        if(result.rows.length === 0){
+           return res.status(404).json({message: "Book not found"});
+        }
+        res.json(result.rows[0]);
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+};
+exports.updateBook = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const {title,author,subject,condition} = req.body;
+        const result = await pool.query(
+            `UPDATE book
+            SET title = $1,author = $2,subject=$3,condition=$4
+            WHERE id=$5
+            RETURNING *`,[title,author,subject,condition,id]
+        );
+        if(result.rows.length === 0){
+            return res.status(404).json({message: "Book not found"});
+        }
+        res.json(result.rows[0]);
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+};
--- Backend/routes/bookRoutes.js
+++ Backend/routes/bookRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {addBook,getBook,deleteBook} = require("../controllers/bookcontroller");
+const {addBook,getBook,deleteBook,getBookById,updateBook} = require("../controllers/bookcontroller");
 router.post("/",addBook);
 router.get("/",getBook);
+router.get("/:id",getBookById);
+router.put("/:id",updateBook);
 router.delete("/:id",deleteBook);
 module.exports = router;
