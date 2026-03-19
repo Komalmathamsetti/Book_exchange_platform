3a3c97c38c67e2f37d8f7cd8482c7a63805ebbf2
Komal Mathamsetti <komalmathamsetti@gmail.com>
2026-03-19 11:42:56 +0530

    Update commits

--- Backend/Backend/routes/exchangeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {createExchangeRequest,getAllRequests,updateExchangeRequest, deleteExchangeRequest} = require("../controllers/exchange_controller");
-router.post("/",createExchangeRequest);
-router.get("/",getAllRequests);
-router.put("/:id",updateExchangeRequest);
-router.delete("/:id",deleteExchangeRequest);
-module.exports = router;
--- Backend/Backend/routes/notificationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getNotifications,getUnreadCount,markAsRead} = require("../controllers/notification_controller");
-router.get("/:user_id",getNotifications);
-router.get("/unread/:user_id",getUnreadCount);
-router.get("/read/:id",markAsRead);
-module.exports = router;
