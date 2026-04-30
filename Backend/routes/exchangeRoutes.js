4db7b2fd265ad49cf93c31e2526a6344e2acfb78
Komal Mathamsetti <komalmathamsetti@gmail.com>
2026-04-30 18:56:34 +0530

    updated a minor change

--- Backend/routes/exchangeRoutes.js
+++ Backend/routes/exchangeRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const {createExchangeRequest,getAllRequests,updateExchangeRequest, deleteExchangeRequest} = require("../controllers/exchange_controller");
 router.post("/",createExchangeRequest);
 router.get("/",getAllRequests);
+router.put("/accept/:id", acceptRequest);
+router.put("/reject/:id", rejectRequest);
 router.put("/:id",updateExchangeRequest);
 router.delete("/:id",deleteExchangeRequest);
 module.exports = router;
